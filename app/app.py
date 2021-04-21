dcc8510d0d4380b37f1a93020ddaf0540214ca79
antoan2 <antoine.biard.10@gmail.com>
2021-04-21 14:15:15 +0200

    First commit

---
+++ app/app.py
@@ -0,0 +1,17 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.express as px
+import pandas as pd
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+
+app.layout = html.Div(children=[
+        html.H1(children='Le Grand Dashbat'),
+                ])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
