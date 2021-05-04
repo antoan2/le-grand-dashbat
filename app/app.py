d23ec6277fe35249267ce891a2b832de9444cf5c
jmunozz <jmunoz@student.42.fr>
2021-05-04 12:04:49 +0200

    ADD: vscode settings + step 1.2

--- app/app.py
+++ app/app.py
@@ -6,12 +6,22 @@ import pandas as pd
 
 external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 
+
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 
 app.layout = html.Div(
     children=[
         html.H1(children="Le Grand Dashbat"),
+        html.Div("Ceci est une div"),
+        dcc.Checklist(
+            options=[
+                {"label": "option 1", "value": "1"},
+                {"label": "option 2", "value": "2"},
+                {"label": "option 3", "value": "3", "disabled": True},
+            ],
+            value=["2"],
+        ),
     ]
 )
 
