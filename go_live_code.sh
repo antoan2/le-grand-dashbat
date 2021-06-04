a5d6843d1a8259c2738aeff6b22a3577b3ae5760
antoan2 <antoine.biard.10@gmail.com>
2021-06-04 10:39:56 +0200

    update main path

--- go_live_code.sh
+++ go_live_code.sh
@@ -4,6 +4,6 @@ git rm dashbat/bar_graph.py
 git rm dashbat/figures.py
 mv dashbat/main_template.py dashbat/main.py
 
-git add main.py
+git add dashbat/main.py
 git rm dashbat/main_template.py
 git commit -m 'Go live code'
